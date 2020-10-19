dfca82e2f97c9a7bafdf15227e3ba5cd65caaed1
isidoreduc <isidoreducasse79@gmail.com>
2020-10-19 11:31:34 +0200

    seeded database

--- DatingApp.API/Startup.cs
+++ DatingApp.API/Startup.cs
@@ -78,12 +78,12 @@ namespace DatingApp.API
           ValidateAudience = false
         };
       });
-
+      services.AddTransient<SeedData>();
 
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env, SeedData seeder)
     {
       if (env.IsDevelopment())
       {
@@ -105,7 +105,8 @@ namespace DatingApp.API
           });
         });
       }
-
+      seeder.SeedUsers();
+      
       app.UseCors(x => x.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
       app.UseHttpsRedirection();
 
