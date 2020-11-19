522ab5933f21cc36991b0b854b858495539e1f72
isidoreduc <isidoreducasse79@gmail.com>
2020-11-19 11:33:27 +0100

    Like entity for managing well likes

--- DatingApp.API/Models/Data/DataContext.cs
+++ DatingApp.API/Models/Data/DataContext.cs
@@ -12,5 +12,24 @@ namespace DatingApp.API.Models.Data
     public DbSet<Value> Values { get; set; }
     public DbSet<User> Users { get; set; }
     public DbSet<Photo> Photos { get; set; }
+    public DbSet<Like> Likes { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      // Like entity is set to has a combination of LikerId and LikeeId as primary key,
+      // because we want a Liker to only like a Likee exactly once.
+      builder.Entity<Like>().HasKey(k => new { k.LikerId, k.LikeeId });
+      // define many to many relationship: one likee can have multiple likers
+      builder.Entity<Like>().HasOne(u => u.Likee)
+        .WithMany(u => u.Likers)
+        .HasForeignKey(u => u.LikeeId)
+        .OnDelete(DeleteBehavior.Restrict);
+      // and viceversa
+      builder.Entity<Like>().HasOne(u => u.Liker)
+        .WithMany(u => u.Likees)
+        .HasForeignKey(u => u.LikerId)
+        .OnDelete(DeleteBehavior.Restrict);
+
+    }
   }
 }
